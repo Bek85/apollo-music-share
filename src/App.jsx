2d2e8444e37f894497b4e75c278ba4df51e2dd28
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-01-03 15:21:06 +0500

    add additional packages for mui for custom styling and create component boilerplates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import "./App.css";
-import Button from "@mui/material/Button";
+import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+
+import Header from "./components/Header";
+import AddSong from "./components/AddSong";
+import SongPlayer from "./components/SongPlayer";
+import SongList from "./components/SongList";
+import { Grid } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="contained">Hello React</Button>
-    </div>
+    <>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <Header />
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={7}>
+              <AddSong />
+              <SongList />
+            </Grid>
+            <Grid item xs={12} md={5}>
+              <SongPlayer />
+            </Grid>
+          </Grid>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { HeadsetTwoTone } from "@mui/icons-material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    marginLeft: theme.spacing(2),
+  },
+}));
+
+import React from "react";
+
+export default function Header() {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar color="secondary" position="fixed">
+        <Toolbar>
+          <HeadsetTwoTone />
+          <Typography className={classes.title} variant="h6" component="h1">
+            Apollo Music Share
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
